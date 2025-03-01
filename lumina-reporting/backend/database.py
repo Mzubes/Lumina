d09e9659bac39f25cc91a36d1ee20832cfe86615
Mzubes <mikhail.zubairov@gmail.com>
2025-02-28 19:26:00 -0500

    Update database.py

--- lumina-reporting/backend/database.py
+++ lumina-reporting/backend/database.py
@@ -1,1 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from config import Config
 
+engine = create_engine(Config.SQLALCHEMY_DATABASE_URI, convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+
+def init_db():
+    import models  # Import all models here
+    models.Base.metadata.create_all(bind=engine)
