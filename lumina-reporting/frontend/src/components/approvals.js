8dad44d051bd86ffc76a24da5fc55cb9fb411c56
Mzubes <mikhail.zubairov@gmail.com>
2025-02-28 19:39:46 -0500

    Update approvals.js

--- lumina-reporting/frontend/src/components/approvals.js
+++ lumina-reporting/frontend/src/components/approvals.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const Approvals = () => {
+  return (
+    <div className="approvals">
+      <h1>Approvals</h1>
+      <p>Approval workflow functionality coming soon...</p>
+    </div>
+  );
+};
+
+export default Approvals;
