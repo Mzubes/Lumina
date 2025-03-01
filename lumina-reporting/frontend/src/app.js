32926f916e096eb5082330af6c072016200deb28
Mzubes <mikhail.zubairov@gmail.com>
2025-02-28 19:38:20 -0500

    Update app.js

--- lumina-reporting/frontend/src/app.js
+++ lumina-reporting/frontend/src/app.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './pages/Login';
+import ClientPortal from './pages/ClientPortal';
 
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/login" component={Login} />
+        <Route path="/client-portal" component={ClientPortal} />
+        {/* Add other routes as needed */}
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
