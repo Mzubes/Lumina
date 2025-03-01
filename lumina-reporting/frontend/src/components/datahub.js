f9ed24456d17b57658680dd703b0bea8a7265d3b
Mzubes <mikhail.zubairov@gmail.com>
2025-02-28 19:39:08 -0500

    Update datahub.js

--- lumina-reporting/frontend/src/components/datahub.js
+++ lumina-reporting/frontend/src/components/datahub.js
@@ -1,1 +1,24 @@
+import React, { useState, useEffect } from 'react';
 
+const DataHub = () => {
+  const [funds, setFunds] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/funds')
+      .then(res => res.json())
+      .then(data => setFunds(data));
+  }, []);
+
+  return (
+    <div className="data-hub">
+      <h1>Data Hub</h1>
+      <ul>
+        {funds.map(fund => (
+          <li key={fund.id}>{fund.name} - {fund.asset_class}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default DataHub;
