8fe0dc1d3c0a9e8e6d73d1bc90da101b1a38a751
Mzubes <mikhail.zubairov@gmail.com>
2025-02-28 19:40:50 -0500

    Update clientportal.js

--- lumina-reporting/frontend/src/pages/clientportal.js
+++ lumina-reporting/frontend/src/pages/clientportal.js
@@ -1,1 +1,13 @@
+import React from 'react';
 
+const ClientPortal = () => {
+  return (
+    <div className="client-portal">
+      <h1>Client Portal</h1>
+      <p>Welcome! Here are your reports:</p>
+      {/* List or cards for client reports */}
+    </div>
+  );
+};
+
+export default ClientPortal;
