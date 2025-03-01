ab7aa0c1834956ed9368d719872555b7e10fbac8
Mzubes <mikhail.zubairov@gmail.com>
2025-02-28 19:40:05 -0500

    Update Distribution.js

--- lumina-reporting/frontend/src/components/Distribution.js
+++ lumina-reporting/frontend/src/components/Distribution.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const Distribution = () => {
+  return (
+    <div className="distribution">
+      <h1>Distribution</h1>
+      <p>Distribution functionality coming soon...</p>
+    </div>
+  );
+};
+
+export default Distribution;
