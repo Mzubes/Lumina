56b57e850c375d1f5b8005923246fda04730caca
Mzubes <mikhail.zubairov@gmail.com>
2025-02-28 19:39:28 -0500

    Update reports.js

--- lumina-reporting/frontend/src/components/reports.js
+++ lumina-reporting/frontend/src/components/reports.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const Reports = () => {
+  return (
+    <div className="reports">
+      <h1>Reports</h1>
+      <p>Reports functionality coming soon...</p>
+    </div>
+  );
+};
+
+export default Reports;
